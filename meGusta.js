1432d746fdf3bfcec1ea3b161fa8c0970b8f162a
Pato.rc <reyespatricio202@gmail.com>
2024-09-30 21:56:23 -0300

    2da parte, funcionalidad

--- meGusta.js
+++ meGusta.js
@@ -1,6 +1,13 @@
-var getLikeButton = document.getElementById("likeButton");
-var getLikeCount = document.getElementById("likeCount");
+var getLikeButtons = document.querySelectorAll(".likeButton");
+var getLikeCounts = document.querySelectorAll(".likeCount");
 
-getLikeButton.addEventListener("click", function() {
-    getLikeCount.innerText = parseInt(getLikeCount.innerText) + 1;
+
+getLikeButtons.forEach(function(getLikeButton){
+    getLikeButton.addEventListener("click", function() {
+        for(var i=0; i<getLikeCounts.length; i++){
+            if(getLikeCounts[i].id == getLikeButton.name){
+                getLikeCounts[i].innerText = parseInt(getLikeCounts[i].innerText) + 1;
+            }
+        };
+    });
 });
